d19f88dd8ff4b413b1b82900de32468976346c6f
Palomino34 <dat.tran@ghielectronics.com>
2022-01-04 14:27:28 -0500

    empty main

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-braingamer.onEvent(GamerButton.down, function () {
-    braingamer.Beep()
-})
-braingamer.onEvent(GamerButton.up, function () {
-    basic.showString("u")
-})
 basic.forever(function () {
-    basic.showNumber(braingamer.Rocket(GamerRocket.Y))
+	
 })
